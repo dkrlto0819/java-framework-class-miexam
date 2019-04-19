7f5ba9bea982882237de72d8f05ae26095a42ca8
dkrlto0819 <enqn_k@naver.com>
2019-04-19 20:25:09 +0900

    [13]

--- src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
+++ src/main/java/kr/ac/jejunu/userdao/DaoFactory.java
@@ -3,6 +3,7 @@ package kr.ac.jejunu.userdao;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import javax.sql.DataSource;
@@ -25,6 +26,11 @@ public class DaoFactory {
     }
 
     @Bean
+    public jejuJdbcTemplate jdbcTemplate(){
+        return new jejuJdbcTemplate(dataSource());
+    }
+
+    @Bean
     public DataSource dataSource(){
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
 
@@ -42,7 +48,7 @@ public class DaoFactory {
 
     @Bean
     public UserDao userDao(){
-        return new UserDao(jdbcContext());
+        return new UserDao(jdbcTemplate());
     }
 
     @Bean
--- src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -1,40 +1,52 @@
 package kr.ac.jejunu.userdao;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
-    //ConnectionMaker connectionMaker;
+    //JdbcContext jdbcContext;
+    jejuJdbcTemplate jdbcTemplate;
 
-    Connection connection;
-    DataSource dataSource;
-    JdbcContext jdbcContext;
-
-    public UserDao(JdbcContext jdbcContext){
-        this.jdbcContext = jdbcContext;
+    public UserDao(jejuJdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public User get(Long id) throws ClassNotFoundException, SQLException {
         String sql = "select * from userinfo where id = ?";
-        Object[] params = new Object[] {id};
-        return jdbcContext.get(sql, params);
+        Object[] params = new Object[]{id};
+        User result = null;
+        try {
+            result = jdbcTemplate.queryForObject(sql, params, (rs, rowNum) -> {
+                User user = new User();
+                user.setId(rs.getLong("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+                return user;
+            });
+        } catch (EmptyResultDataAccessException e) {
+
+        }
+        return result;
     }
 
     public Long add(User user) throws ClassNotFoundException, SQLException {
         String sql = "insert into userinfo(name, password) values(?, ?)";
         Object[] params = new Object[] {user.getName(), user.getPassword()};
-        return jdbcContext.add(sql, params);
+        return jdbcTemplate.insert(sql, params);
     }
 
     public void update(User user) throws SQLException {
         String sql = "update userinfo set name = ?, password = ? where id = ?";
         Object[] params = new Object[] {user.getName(), user.getPassword(), user.getId()};
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 
     public void delete(Long id) throws SQLException {
         String sql = "delete from userinfo where id = ?";
         Object[] params = new Object[] {id};
-        jdbcContext.update(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 }
